50da00b3d289d44213bf40b081be8cc94f86f304
Abiaina <abinnet.a@gmail.com>
2018-07-09 21:44:29 -0700

    added more logic into the webscrapping for seed data

--- webscrapping/getCompanyDetails.py
+++ webscrapping/getCompanyDetails.py
@@ -3,6 +3,9 @@ from unidecode import unidecode
 import requests
 import json
 from BeautifulSoup import BeautifulSoup
+import untangle
+
+# do I need this?
 import ./companies.csv
 
  ## get company name
@@ -15,8 +18,10 @@ key = '&apikey=' + OSKey
 companyDetails = []
 
 for row in csv_f:
-
  ## make api call for getId
- response = requests.get(url).json()
+ obj = untangle.parse(url + row + key)
+    id = obj.response.organization['orgid']
  ## get company info from orgId
+    objSite = untangle.parse('https://www.opensecrets.org/api/?method=orgSummary&id=' + id + key)
  ## webscrape info from site
+        objSite
