3e3d76b612144c42d348f52a966bc2d38572845d
Abiaina <abinnet.a@gmail.com>
2018-07-12 15:44:29 -0700

    added logic for seed json read file to make db

--- company-politics-api/db/seeds.rb
+++ company-politics-api/db/seeds.rb
@@ -1,40 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
+require 'json'
 
-#make it ruby
-f = open('company_data.json')
-csv_f = csv.reader(f)
-
-company_data_objects = []
-
-company_data_objects.each |json| do
-  name = [json :company_name]
-  lobbying_dollars = [json :lobbying_dollars]
-  contribution_dollars = [json :contribution_dollars]
-
-  # hash with name and description keys
-  most_lobbied_bill = [json :most_lobbied_bill]
-  # array of hashes with name keys
-  share_holders = [json :company_share_holders]
+json_data = JSON.parse(File.read('./seed_data.json'))
 
 
-  company = Company.create([{ name: name }, { lobbying_dollars: lobbying_dollars }, {contribution_dollars: contribution_dollars}])
+json_data.each |json| do
+  name = json["name"]
+  lobbying_dollars = ["total_lobby_dollars"]
+  contribution_dollars = json["total_contribution_dollars"]
+  most_lobbied_bill = json["most_lobbied_bill"]
+  share_holders = json["share_holder_politicians"]
+  top_recipients = json["recipients"]
+  os_id = json["os_id"]
 
-# check if these values are not nill or none
-# check if these instances already exist
+
+  company = Company.create([{ name: name }, { lobbying_dollars: lobbying_dollars }, {contribution_dollars: contribution_dollars}, { os_id: os_id}])
 
 # these just check for if the instances already exist
-  if !most_lobbied_bill.find_by(:name [json :most_lobbied_bill]) do
-    company = MostLobbiedBill.create([{ name: name }, { description: [json :description }])
+  if most_lobbied_bill.find_by(:name most_lobbied_bill) do
+    company.most_lobbied_bill = most_lobbied_bill.find_by(:name most_lobbied_bill)
+  else
+    bill = MostLobbiedBill.create([{ name: most_lobbied_bill }, { description: ""})
+    company.most_lobbied_bill = bill
   end
 
-  if !share_holders.find_by(:name share_holders) do
-    company = MostLobbiedBill.create([{ name: name }])
+  share_holders.each |s_holder| do
+    politician = share_holders.find_by(:name s_holder)
+
+    if !politician do
+      politician = share_holders.create([{ name: s_holder }])
+    end
+
+    company.share_holders = politician
   end
 
-  if !top_recipients.find_by(:name top_recipients) do
-    company = MostLobbiedBill.create([{ name: name }])
+  top_recipients.each |t_recipient| do
+    taker = top_recipients.find_by(:name t_recipient)
+
+    if !taker do
+      taker = share_holders.create([{ name: t_recipient }])
+    end
+    company.top_recipients = taker
   end
 end
