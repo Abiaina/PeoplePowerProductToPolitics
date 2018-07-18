4b0e91453b5b2f29011a8decef93f0c09d2ff1bb
Abiaina <abinnet.a@gmail.com>
2018-07-18 00:30:10 -0700

    subsidiaries working on test case and oputs json to json file

--- webscrapping/SubsidaryScrapLogic.py
+++ webscrapping/SubsidaryScrapLogic.py
@@ -15,25 +15,34 @@ def get_summary_data(site):
     soup = BeautifulSoup(html)
 
     # top recipients (remember to remove the last element in the array)
-    table = soup.findAll("table", attrs={'class': 'datadisplay'})[1]
+    info_table = soup.findAll("table", attrs={'class': 'datadisplay'})[-1]
+    if info_table.find('tr').find('th').text.lower() == "affiliate":
+        for row in info_table.findAll('tr'):
+            list_of_cells = []
+            # going looking for a link tag in the row
+            for cell in row.findAll('td'):
+                # this is checking the link tag for any non ascii char
+                text = unidecode(cell.text)
+                text = text.replace('&nbsp;', '')
+                list_of_cells.append(text)
+            if list_of_cells:
+                subsidiaries.append(list_of_cells[0])
 
-    # this going row by row
-    for row in table.findAll('tr'):
-        list_of_cells = []
-        # going looking for a link tag in the row
-        for cell in row.findAll('td'):
-            # this is checking the link tag for any non ascii char
-            text = unidecode(cell.text)
-            text = text.replace('&nbsp;', '')
-            list_of_cells.append(text)
-        if list_of_cells:
-            subsidiaries.append(list_of_cells[0])
+        data = {
+            'subsidiaries': subsidiaries,
+        }
 
-    data = {
-        'subsidiaries': subsidiaries,
-    }
+        outfile = "./unfound_companies.json"
 
-    print "data is here"
-    print data
+        with open(outfile, 'w') as outfile:
+            json.dump(data, outfile, sort_keys = True, indent = 4,
+               ensure_ascii = False)
 
-get_summary_data('https://www.opensecrets.org/orgs/totals.php?id=D000027084&cycle=2018')
+data = get_summary_data('https://www.opensecrets.org/orgs/totals.php?id=D000027084&cycle=2018')
+
+# # data = json.dumps(data)
+# outfile = open("./unfound_companies.csv", "wb")
+# writer = csv.writer(outfile)
+# writer.writerow(data)
+
+print data
